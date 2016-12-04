5766522d2571bba828dac703207722371f3ec9e8
HatemMorgan <hatemmorgan17@gmail.com>
2016-12-04 09:40:27 +0200

    adding events activity and its layout

---
+++ app/src/main/java/com/example/hatem/tick_toc_app/EventsActivity.java
@@ -0,0 +1,47 @@
+package com.example.hatem.tick_toc_app;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+
+public class EventsActivity extends AppCompatActivity {
+
+    Toolbar toolbar ;
+    ListView listView ;
+    Context context
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+
+
+        listView = (ListView) findViewById(R.id.events_listview);
+        context = this;
+        initToolBar();
+
+    }
+
+
+    private void initToolBar (){
+        toolbar = (Toolbar) findViewById(R.id.events_toolbar);
+        toolbar.setTitle("Events");
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+}
