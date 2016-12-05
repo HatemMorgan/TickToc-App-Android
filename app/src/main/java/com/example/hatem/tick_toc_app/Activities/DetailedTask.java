7a42c30e70f63877aa2d2e95adf52932144fa71d
HatemMorgan <hatemmorgan17@gmail.com>
2016-12-05 21:25:17 +0200

    adding detailed tasks Object relation model

--- app/src/main/java/com/example/hatem/tick_toc_app/Activities/DetailedTask.java
+++ app/src/main/java/com/example/hatem/tick_toc_app/Activities/DetailedTask.java
@@ -1,7 +1,9 @@
 package com.example.hatem.tick_toc_app.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hatem.tick_toc_app.R;
 
 public class DetailedTask extends AppCompatActivity {
 
